6e90ccfe1b3a83d15a3c0932e14c098ee5830848
ojikeii <ojikeii@gmail.com>
2021-01-18 22:37:59 +0900

    modify a solution

--- 153-find-minimum-in-rotated-sorted-array.php
+++ 153-find-minimum-in-rotated-sorted-array.php
@@ -7,34 +7,36 @@ class Solution {
      * @return Integer
      */
     function findMin($nums) {
-        // $prev = -5001;
-        // foreach($nums as $num) {
-        //     if ($num < $prev) return $num;
-        //     $prev = $num;
-        // }
-        // return $nums[0];
-        
-        $left = 0;
-        $right = count($nums) - 1;
-      
-        while($left < $right) {
-            $leftVal = $nums[$left];
-            $rightVal = $nums[$right];
-            if ($leftVal < $rightVal) return $leftVal;
-
-            $middle = $left + intdiv($right - $left, 2);
-            $middleVal = $nums[$middle];
-            if ($middle > 0 && $middleVal < $nums[$middle - 1]) {
-                 return $middleVal;
-            }
-
-            if ($middle >= $leftVal) {
-                $left = $middle + 1;
-            } else {
-                $right = $middle - 1;
-            }
+        // simple
+        $prev = -5001;
+        foreach($nums as $num) {
+            if ($num < $prev) return $num;
+            $prev = $num;
         }
+        return $nums[0];
         
-        return $nums[$left];
+        // binary search
+        // $left = 0;
+        // $right = count($nums) - 1;
+      
+        // while($left < $right) {
+        //     $leftVal = $nums[$left];
+        //     $rightVal = $nums[$right];
+        //     if ($leftVal < $rightVal) return $leftVal;
+
+        //     $middle = $left + intdiv($right - $left, 2);
+        //     $middleVal = $nums[$middle];
+        //     if ($middle > 0 && $middleVal < $nums[$middle - 1]) {
+        //          return $middleVal;
+        //     }
+
+        //     if ($middleVal >= $leftVal) {
+        //         $left = $middle + 1;
+        //     } else {
+        //         $right = $middle - 1;
+        //     }
+        // }
+        
+        // return $nums[$left];
     }
 }

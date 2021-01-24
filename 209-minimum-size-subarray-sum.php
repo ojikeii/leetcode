15c9b87199a9515ba7754a1648972befda29e8c4
ojikeii <ojikeii@gmail.com>
2021-01-24 15:56:10 +0900

    add a solution

---
+++ 209-minimum-size-subarray-sum.php
@@ -0,0 +1,31 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer $s
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function minSubArrayLen($s, $nums) {
+        $N = count($nums);
+        if ($N === 0) return 0;
+        
+        $min = $sum = $right = $left = 0;
+        $sum = $nums[$right];
+        while ($right < $N && $left <= $right) {
+            if ($sum >= $s) {
+                $min = $right - $left + 1;
+            } else {
+                $right++;
+                $sum += $nums[$right];
+            }
+            if ($min > 0) {
+                $sum -= $nums[$left];
+                $left++;
+            }
+        }
+
+        return $min;
+    }
+}

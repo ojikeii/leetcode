55e176883035890dd150ca01c766b030d2c00180
ojikeii <ojikeii@gmail.com>
2021-01-30 19:24:20 +0900

    modify a solution

--- 198-house-robber.php
+++ 198-house-robber.php
@@ -11,7 +11,29 @@ class Solution
     {
         $count = count($nums);
         if ($count === 0) return 0;
+        if ($count === 1) return $nums[0];
 
+        // DP solution(memory saved)
+        $prevmax = $nums[0];
+        $curmax = max($nums[0], $nums[1]);
+        for ($i = 2; $i < $count; $i++) {
+            $temp = $curmax;
+            $curmax = max($curmax, $prevmax + $nums[$i]);
+            $prevmax = $temp;
+        }
+        return $curmax;
+
+        // DP solution
+        /*
+        $dp = [$nums[0], max($nums[0], $nums[1])];
+        for ($i = 2; $i < $count; $i++) {
+            $dp[$i] = max($dp[$i - 1], $dp[$i - 2] + $nums[$i]);
+        }
+        return $dp[$count - 1];
+        */
+
+        // poor DP solution
+        /*
         $dp = [$nums[0]];
         $max = $dp[0];
         for ($i = 1; $i < $count; $i++) {
@@ -22,7 +44,7 @@ class Solution
             $dp[$i] = $prevmax + $nums[$i];
             $max = max($max, $dp[$i]);
         }
-
         return $max;
+        */
     }
 }

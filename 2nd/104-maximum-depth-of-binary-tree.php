4cc68e6c46518080bddfc3f5fb684cbce9d13815
ojikeii <ojikeii@gmail.com>
2021-03-22 21:23:28 +0900

    update a solution

--- 2nd/104-maximum-depth-of-binary-tree.php
+++ 2nd/104-maximum-depth-of-binary-tree.php
@@ -22,11 +22,33 @@ class Solution
      */
     function maxDepth($root, $depth = 0)
     {
+        // DFS
         if ($root === null) return $depth;
 
         return max(
             $this->maxDepth($root->left, $depth + 1),
             $this->maxDepth($root->right, $depth + 1)
         );
+
+        // BFS
+        /*
+        if ($root === null) return 0;
+        
+        $depth = -1;
+        $queue = [$root];
+        while (!empty($queue)) {
+            $count = count($queue);
+            for ($i = 0; $i < $count; $i++) {
+                $node = array_shift($queue);
+                if ($node !== null) {
+                    $queue[] = $node->left;
+                    $queue[] = $node->right;
+                }
+            }
+            $depth++;
+        }
+        
+        return $depth;
+        */
     }
 }
